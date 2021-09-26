9839314287c60ae1a7c997e3a4db8458f24e4548
Mihir Kondapalli <32917040+Immortal747@users.noreply.github.com>
2021-09-25 20:27:48 -0700

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,12 +3,12 @@ from discord.ext import commands
 
 token = open("token.txt", "r").read()
 
-PREFIX = '-G '
+PREFIX = '-g '
 
 client = commands.Bot(command_prefix = PREFIX)
 
 def begins(text, start):
-    return text.lower().startswith(PREFIX+start)
+    return text.content.lower().startswith(PREFIX+start)
 
 @client.event
 async def on_ready():
@@ -16,15 +16,9 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    text = message.content
-
-    if begins(text, 'ping'):
+    
+    if begins(message, 'ping'):
         await message.channel.send(f'Pong! {round(client.latency * 1000)}ms')
-
-    if begins(text, 'find '):
-        tokens = text.split()
-        member = discord.utils.find(lambda m: m.name == tokens[1], channel.guild.members)
-        await message.channel.send(member)
 
 
 client.run(token)
