ae23f530377f92bdd49b13ded85f82053eb26971
Mihir Kondapalli <32917040+Immortal747@users.noreply.github.com>
2021-09-25 21:53:19 -0700

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+from URLReader import URLReader
+
+import random
 
 token = open("token.txt", "r").read()
 
@@ -20,5 +23,30 @@ async def on_message(message):
     if begins(message, 'ping'):
         await message.channel.send(f'Pong! {round(client.latency * 1000)}ms')
 
+    if begins(message, 'typerace'):
+        words = message.content.split()
+
+        if len(words)<3:
+            await message.channel.send('https://www.nitrotype.com/race/'+message.author.name)
+            return
+
+        await message.channel.send('https://www.nitrotype.com/race/'+words[2])
+
+    if begins(message, 'chess'):
+        words = message.content.split()
+        await message.channel.send('https://www.chess.com/play/'+words[2])
+
+    if begins(message, 'dice'):
+        words = message.content.split()
+
+        if len(words) < 3:
+            await message.channel.send(str(int(random.random()*6)+1))
+            return
+
+        if words[2].isnumeric() and int(words[2])>=2 and int(words[2])<=1000000:
+            await message.channel.send(str(int(random.random()*int(words[2]))+1))
+            return
+
+        await message.channel.send('Give number of sides [2, 1000000]')
 
 client.run(token)
